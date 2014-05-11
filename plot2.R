27275684be4ffd300767f937cd7167d6bf911d4e
sugzoom <chadharness@gmail.com>
2014-05-11 18:31:55 -0400

    Adding plot2.R and plot2.png

---
+++ plot2.R
@@ -0,0 +1,10 @@
+##First, download the file from the data repository and unzip it into your working directory.
+mySql <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
+filename = "./household_power_consumption.txt"
+dt1 <- read.csv.sql(filename,mySql,header=TRUE,sep=";")
+dt1$DateTime <- strptime(paste(dt1$Date, dt1$Time, sep=" "), format="%d/%m/%Y %H:%M:%S")
+
+
+png(filename="plot2.png",width=480,height=480)
+with(dt1,plot(x=DateTime,y=Global_active_power,type="l",ylab="Global Active Power (kilowatts)"))
+dev.off()
